759c7f001b9f40688347d49c6a2972dcd253a28d
john <johnpwoodiv@gmail.com>
2017-10-24 22:57:56 -0500

    completed javascript toy problems

--- toyProblems.js
+++ toyProblems.js
@@ -1,27 +1,44 @@
 //JS Basics
 //Run npm test in the command line to test your solutions
 module.exports = {
-  // reverseIt:
-
+ 
+  
   //uncomment and finish the reverseIt function. It will take in one parameter which is a String and
-  //reverse it
+    reverseIt:function r(s){return s===""?s:s[s.length-1]+r(s.slice(0,s.length-1));},
 
-  // removeDups:
-
+    removeDups:function(a){
+	for(let i=0;i<a.length;i++){
+	    if(a.indexOf(a[i])!==i){
+		a.splice(i,1);
+		i--;
+	    }
+	}
+	return a;
+    },
+    
   //uncomment and finish the removeDups function. It will take in one parameter which is an Array
   //remove all duplicates
 
-  // titleIt:
+    titleIt:function(s){return s.split(" ").map(x=>x[0].toUpperCase()+x.slice(1,x.length)).join(" ");},
 
   //uncomment and finish the titleIt function. It will take in one parameter which is a String and
   //capitalize the first letter of each word
 
-  // vowelCounter:
+    vowelCounter:function(s){
+	return s.match(/[aeiou]/gi).length;
+    },
 
   //uncomment and finish the vowelCounter function. It will take in one parameter which is a String and
   //return the number of vowels in the string
 
-  // isPrime:
+    isPrime:function(n){
+	if(typeof n !== 'number'){return false;}
+	if(n<2){return false;}
+	for(let i=2;i<n;i++){
+	    if(n%i===0){return false;}
+	}
+	return true;
+    },
 
   //uncomment and finish the isPrime function. It will take in one parameter which is a Number and
   //return true if it is prime and false if it is not
@@ -30,7 +47,7 @@ module.exports = {
   //var foo = 10 + '20';
   //uncomment the foo property and give your answer as its value
 
-  //foo:
+    foo:'1020',
 
   //what is the outcome of the two console.logs below?
   //   var foo = "Hello";
@@ -41,6 +58,8 @@ module.exports = {
   // console.log(foo + bar);
   // uncomment the log1 and log2 properties and give your answers as their values
 
-  // log1:
-  // log2:
+    log1:'Hello World',
+    log2:'Helloundefined'
 }
+
+
